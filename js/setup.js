dcb4859d6c23050d93e2959ffc782deb75a148fd
JD <jaydeepc@thoughtworks.com>
2016-12-11 12:31:32 +0530

    JD: Modified setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   $("#stage_spec").hide();
+  $(".summary-page-table-heading").text("Complete");
+
+  //Setup execution table column details
+  $("#first-details").text("Platform");
+  $("#second-details").text("Python Version");
+  $("#third-details").text("Execution Time");
+  $("#fourth-details").text("Created On");
 });
