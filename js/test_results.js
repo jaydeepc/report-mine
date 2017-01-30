8bbe8465d4c12336f0d1e8ae6dc0bc258548a28d
JD <jaydeepc@thoughtworks.com>
2017-01-30 17:46:15 +0530

    JD: Optimized the code a bit. Needs CSS changes

--- js/test_results.js
+++ js/test_results.js
@@ -1,39 +1,26 @@
-$(document).ready(function() {
+$(window).load(function() {
 
     $.getJSON('../data/result.json', function(jd) {
 
         test_list = jd.report.tests;
         for(i=0; i<test_list.length; i++){
             if (test_list[i].outcome == "failed"){
-                $(".container tbody").append("<tr><td>" + fetch_module_name(test_list[i].name)
+                $(".container tbody").append("<tr id='table-row-"+i+"'><td>" + fetch_module_name(test_list[i].name)
                 +
                 "</td><td>"
                 +
                 fetch_test_name(test_list[i].name)
                 +
-                "</td><td><a class='md-trigger' data-modal='modal-12' onclick='showModal();' name='"+ fetch_test_name(test_list[i].name) +"'>Show Failure Trace</a></td><td>"
+                "</td><td><a onclick='showModal("+i+");'>Show Failure Trace</a></td><td>"
                 +
                 has_artifact()
                 +
                 "</td></tr>");
-
-              $( ".md-trigger" ).promise().done(function() {
-                $("body").append("<div class='md-modal md-effect-12' id='modal-12' name='" + fetch_test_name(test_list[i].name) + "' >" +
-                "<div class='md-content'>" +
-                "<h3>" +
-                "Console Error" +
-                "</h3><hr><br><p><pre>" + test_list[i].call.longrepr.trim() + "</pre></p><br><hr><button class='md-close' onclick='removeModal();'>Close</button></div>" +
-                "</div><div class='md-overlay'></div>")
-
-              });
-
+              }
             }
-        }
+        });
 
     });
-
-
-});
 
 
 function fetch_module_name(name_string){
