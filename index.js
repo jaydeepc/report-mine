c5fe439ae360e609ec7c43aaf3af2260fa60ea6d
saikrisv <saikrisv@insaikrisv-2.local>
2017-02-13 14:25:03 +0530

    added missing dependency and wrapped error

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ var fs_copy = require("fs-extra")
 var Spinner = CLI.Spinner;
 var path = require('path');
 var findRemoveSync = require('find-remove')
-
-
+var colors = require('colors/safe');
 
 console.log(
     chalk.blue(
@@ -28,17 +27,19 @@ global = path.resolve(__dirname);
 
 var status = new Spinner('Generating Reports, please wait...');
 status.start();
-console.log(global)
 fs_copy.copy(global , process.argv[2]+"/report-mine" , function (err) {
     if (err) {
         status.stop();
+        return console.log(colors.red.bold("Please provide the directory of results.json"));
     } else {
         findRemoveSync(process.argv[2]+"/report-mine", {dir: 'node_modules',
             extensions: ['.json', '.md'], files: ['index.js', '.npmignore']})
         if (fs.existsSync(process.argv[2]+"/report-mine/data/")) {
             fs_copy.move(process.argv[2]+"/results.json", process.argv[2]+"/report-mine/data/results.json" , function (err) {
                 if (err) return console.error(err + "Please provide path to results to results.json")
-                console.log("success!")
+                console.log(colors.green.bold("Success!"))
+                console.log(colors.green.bold("To view the results, start a local server and view the summary.html @" +
+                    process.argv[2]+"report-mine/views/summary.html"));
             });
         }
         status.stop();
